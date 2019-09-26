8283328e753f8663d65b4addc2c21360d9595dbd
Jonathan Dorety <kyullog@gmail.com>
2019-09-25 19:18:39 -0700

    Add django-heroku and configure for deploy

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
