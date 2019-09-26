f8af00ed121b3defc1eeaf9947ed41cab65a136c
Jonathan Dorety <kyullog@gmail.com>
2019-09-25 19:42:37 -0700

    Add get player status endpoint

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from adventure.api import AllRoomsViewSet
+from adventure.api import AllRoomsViewSet, PlayerViewSet
 
 router = routers.DefaultRouter()
 router.register(r'allrooms', AllRoomsViewSet)
+router.register(r'init', PlayerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- adventure/api.py
+++ adventure/api.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers, viewsets
-from .models import Room
+from .models import Room, Player
 
 
 class AllRoomsSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,3 +11,21 @@ class AllRoomsSerializer(serializers.HyperlinkedModelSerializer):
 class AllRoomsViewSet(viewsets.ModelViewSet):
     serializer_class = AllRoomsSerializer
     queryset = Room.objects.all()
+
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Player
+        fields = ('uuid', 'currentRoom')
+
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    serializer_class = PlayerSerializer
+    queryset = Player.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return Player.objects.filter()
+        else:
+            return Player.objects.filter(user=user)
